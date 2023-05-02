b9e042efc9522faed7e320a24c9d2b7bad788b6d
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-05-02 09:03:52 -0500

    Add files via upload

---
+++ js/pokemonfav.js
@@ -0,0 +1,23 @@
+class Character {
+    constructor(id, name, image,types) {
+        this.id = id
+        this.name = name
+        this.image = image
+        this.types = types
+      
+    }
+
+    toHtml(pos) {
+
+        return `
+        <div class="card" onclick="selected(${pos})">
+            <p id="nombre">${this.name}</p>
+            <span class="card-icon"><i class="fas fa-trash"></i></i></span> 
+            <img src="${this.image}" />
+            <p id="numero">N°${this.id.toString().padStart(4, '0')}</p>
+            <p id="tiposs">${this.types[0]}/${this.types[1]}</p>
+          
+        </div>
+        `
+    }
+}
