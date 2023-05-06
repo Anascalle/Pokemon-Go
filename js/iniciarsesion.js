9bbe2cea29da6760db39df88b104824354f0cbed
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-05-05 23:09:12 -0500

    Update iniciarsesion.js
    usuario: Pokemon1
    contraseña:Amolospokemones

--- js/iniciarsesion.js
+++ js/iniciarsesion.js
@@ -28,10 +28,20 @@ passwordInput.addEventListener('input', ()=>{
     verificarCampos()
 });
 
-iniciarSesionBtn.addEventListener("click", (e)=>{
-    e.preventDefault();
-    window.location.href = "./Pokedex.html"
-})
+iniciarSesionBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  const usuarioValido = 'Pokemon1'; // 'nombreDeUsuario' por el valor correcto
+  
+  const contrasenaValida = 'Amolospokemones'; // 'contrasenaSegura' por el valor correcto
+  
+  if (usernameInput.value === usuarioValido && passwordInput.value === contrasenaValida) {
+    window.location.href = "./Pokedex.html";
+  } else {
+    alert("Credenciales inválidas. Inténtalo de nuevo.");
+  }
+});
+
 
 
 
