a57fa3bfcfd1a75898a5642aea852fdf7f1d7a08
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-05-06 09:18:35 -0500

    Update pokemon.js

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,22 +1,19 @@
 class Character {
-    constructor(id, name, image,types) {
-        this.id = id
-        this.name = name
-        this.image = image
-        this.types = types
-      
+    constructor(id, name, image, types) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.types = types.filter(Boolean);
     }
 
     toHtml(pos) {
-
         return `
         <div class="card" onclick="selected(${pos})">
             <p id="nombre">${this.name}</p>
             <img src="${this.image}" />
             <p id="numero">N°${this.id.toString().padStart(4, '0')}</p>
-            <p id="tiposs">${this.types[0]}/${this.types[1]}</p>
-          
+            <p id="tiposs">${this.types.join('/')}</p>
         </div>
-        `
+        `;
     }
 }
