a72ce7deb4f86b6bc56acec909d48f3575f3c49f
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-05-05 22:24:30 -0500

    Update pokemonfav.js

--- js/pokemonfav.js
+++ js/pokemonfav.js
@@ -12,7 +12,7 @@ class Character {
         return `
         <div class="card" onclick="selected(${pos})">
             <p id="nombre">${this.name}</p>
-            <span class="card-icon"><i class="fas fa-trash"></i></i></span> 
+            
             <img src="${this.image}" />
             <p id="numero">N°${this.id.toString().padStart(4, '0')}</p>
             <p id="tiposs">${this.types[0]}/${this.types[1]}</p>
