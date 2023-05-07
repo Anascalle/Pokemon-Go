0687af51a92e3abffc85fa8acd6ccb22f2520803
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-05-07 13:02:34 -0500

    Update agregarfavoritos.js

--- js/agregarfavoritos.js
+++ js/agregarfavoritos.js
@@ -6,7 +6,7 @@ const favoritosIcono = document.createElement("i");
     favoritosIcono.onclick = function() {
         if (favoritosIcono.classList.contains('clicked')) {
             favoritosIcono.classList.remove('clicked');
-            alert("Se ha eliminado de favoritos");
+            
         } else {
             favoritosIcono.classList.add('clicked');
             alert("Se ha agregado a favoritos");
