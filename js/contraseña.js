cee39e3f396f26737eac15cd92f0f113e64223e4
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-05-07 18:46:41 -0500

    Update contraseña.js

--- js/contraseña.js
+++ js/contraseña.js
@@ -2,12 +2,12 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  const currentPassword = document.getElementById('current-password').value;
+
   const newPassword = document.getElementById('new-password').value;
   const confirmPassword = document.getElementById('confirm-password').value;
   
   // Validar campos
-  if (currentPassword === '' || newPassword === '' || confirmPassword === '') {
+  if ( newPassword === '' || confirmPassword === '') {
     alert('Todos los campos son requeridos');
     return;
   }
@@ -15,19 +15,7 @@ form.addEventListener('submit', function(event) {
     alert('La nueva contraseña y la confirmación no coinciden');
     return;
   }
-  if (currentPassword === newPassword) {
-    alert('La contraseña actual y la nueva contraseña deben ser diferentes');
-    return;
-  }
-  
-  // Verificar contraseña actual
-  // Aquí puedes hacer una solicitud al servidor para verificar la contraseña actual
-  
-  // Cambiar contraseña
-  // Aquí puedes hacer una solicitud al servidor para cambiar la contraseña
-  
-  // Guardar nueva contraseña en el local storage
-  localStorage.setItem('password', newPassword);
+
   
   // Mostrar mensaje de éxito
   alert('La contraseña ha sido cambiada con éxito');
