2cfc52af64d0e3e8c28262455f4ec598688f7f73
Anascalle <127751671+Anascalle@users.noreply.github.com>
2023-06-02 15:29:29 -0500

    Update Pokedex.js

--- js/Pokedex.js
+++ js/Pokedex.js
@@ -1,36 +1,78 @@
-const results = data;
 const list = [];
 const listFav = [];
-window.localStorage.setItem("favoritos", JSON.stringify(listFav))
+window.localStorage.setItem("favoriteList", JSON.stringify(listFav));
 
 const listContainer = document.getElementById("container");
+const searchInput = document.getElementById("search-input");
+const noResultsMessage = document.getElementById("no-results-message");
 
 function getList() {
-  for (let i = 0; i < results.length; i++) {
-    const data = results[i];
-    const types = [];
+  const apiUrl = 'https://raw.githubusercontent.com/Anascalle/Pokemon-Go/main/js/data1.json';
 
-    if (data.types.length > 0) {
-      types.push(data.types[0].type.name);
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const results = data;
 
-      if (data.types.length > 1) {
-        types.push(data.types[1].type.name);
+      for (let i = 0; i < results.length; i++) {
+        const data = results[i];
+        const types = [];
+
+        if (data.types.length > 0) {
+          types.push(data.types[0].type.name);
+
+          if (data.types.length > 1) {
+            types.push(data.types[1].type.name);
+          }
+        }
+
+        const character = new Character(data.id, data.name, data.sprites.other["official-artwork"].front_default, types);
+        list.push(character);
       }
-    }
 
-    const character = new Character(data.id, data.name, data.sprites.other["official-artwork"].front_default, types);
-    list.push(character);
-  }
+      renderList(list);
 
-  for (let i = 0; i < list.length; i++) {
-    const character = list[i];
-    listContainer.innerHTML += character.toHtml(i);
+      // Guardar los datos en el Local Storage
+      localStorage.setItem('pokemonData', JSON.stringify(list));
+    })
+    .catch(error => {
+      console.log('Error al obtener los datos:', error);
+    });
+}
+
+function renderList(characters) {
+  listContainer.innerHTML = "";
+
+  for (let i = 0; i < characters.length; i++) {
+    const character = characters[i];
+    const cardElement = document.createElement("div");
+    cardElement.className = "card";
+    cardElement.innerHTML = character.toHtml(i);
+    cardElement.addEventListener("click", () => selected(character.id)); // Modificado aquí
+    listContainer.appendChild(cardElement);
   }
 }
 
-function selected(pos) {
-  const character = list[pos];
-  window.location.href = `./detail.html?id=${character.id}`;
+function selected(id) {
+  const character = list.find(item => item.id === id);
+  if (character) {
+    window.location.href = `./detail.html?id=${character.id}`;
+  }
 }
+
+function searchCharacter() {
+  const searchValue = searchInput.value.toLowerCase();
+  const filteredList = list.filter(character => character.name.toLowerCase().includes(searchValue));
+
+  if (filteredList.length > 0) {
+    renderList(filteredList);
+    noResultsMessage.classList.add("hidden");
+  } else {
+    listContainer.innerHTML = "";
+    noResultsMessage.classList.remove("hidden");
+  }
+}
+
+searchInput.addEventListener("input", searchCharacter);
 
 getList();
